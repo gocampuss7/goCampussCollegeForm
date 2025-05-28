0eae731a038adc3856b95b0af5133b2e508d0270
BerrySeriousCoder <harsh100xdev.work@gmail.com>
2025-05-28 23:17:49 +0530

    updated vercel domain and main .com domain in cors origin

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://gocampuss-form.onrender.com","https://gocampuss.vercel.app/","https://gocampuss.com"],
+    origin: ["http://localhost:5173", "https://gocampuss-form.onrender.com","https://gocampuss.vercel.app","https://www.gocampuss.com"],
     methods: ["GET, POST, PUT, DELETE"],
     credentials: true,
   })
