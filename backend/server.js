7f1c0202054279eb5b38683750e6eb09b9e167ab
RUDRA PRATAP SINGH <rudra.2226en1029@kiet.edu>
2025-07-02 09:23:06 +0530

    Fix : origin change for security

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://gocampuss-form.onrender.com","https://gocampuss.vercel.app","https://www.gocampuss.com"],
+    origin: [
+      "https://gocampuss-form.onrender.com",
+      "https://gocampuss.vercel.app",
+      "https://www.gocampuss.com",
+    ],
     methods: ["GET, POST, PUT, DELETE"],
     credentials: true,
   })
